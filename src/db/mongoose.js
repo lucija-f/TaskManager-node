b7d27f9cc1d3102e91e05bf0966c0a07f81b33c5
Luc <racunarka@protonmail.com>
2022-05-04 10:08:24 +0200

    env variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-// mongoose connects to the local database
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager')
+// mongoose connects to the database
+mongoose.connect(process.env.MONGODB_URL)
 
